5851419a7b13263147162c7cc19504745f70df6e
Divyanshu Sahu <257divyanshu@gmail.com>
2025-09-15 17:36:07 +0530

    refactor(cointable): migrate data fetching to use TanStack Query

--- src/components/CoinTable/CoinTable.jsx
+++ src/components/CoinTable/CoinTable.jsx
@@ -1,27 +1,53 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { fetchCoinData } from "../../services/fetchCoinData";
+import { useQuery } from "@tanstack/react-query";
 
 function CoinTable({ currency }) {
+
     const [page, setPage] = useState(1);
-    useEffect(() => {
-        fetchCoinData(page, currency);
-    }, [page, currency]);
-    return (
-        <>
-            <div className="bg-sky-300">
-                CoinTable Component
-            </div>
-            <div>
-                <button
-                    onClick={() => { setPage(page + 1) }}
-                >
-                    Click
-                </button>
-            </div>
-            <div>
-                page no {page}
-            </div>
-        </>
-    )
+
+    const { data, isLoading, isError, error } = useQuery({
+        queryKey: ['coins', page, currency],
+        queryFn: () => fetchCoinData(page, currency),
+        retry: 2,
+        retryDelay: 1000,
+        gcTime: 1000 * 60 * 2,
+        staleTime: 1000 * 60,
+        refetchOnWindowFocus: false
+    });
+
+    if (isLoading) {
+        return (
+            <div>loading data</div>
+        )
+    };
+
+    if (isError) {
+        return (
+            <div>Error: {error.message}</div>
+        )
+    };
+
+    if (data) {
+        return (
+            <>
+                <div className="bg-sky-300">
+                    CoinTable Component
+                </div>
+                <div>
+                    <button
+                        onClick={() => { setPage(page + 1) }}
+                    >
+                        Click
+                    </button>
+                </div>
+                <div>
+                    page no {page}
+                </div>
+            </>
+        )
+    }
+
 }
+
 export default CoinTable;
