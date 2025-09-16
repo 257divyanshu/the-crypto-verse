1c63f3a8b170d226200d3d720a4b69f1b0a29d15
Divyanshu Sahu <257divyanshu@gmail.com>
2025-09-16 10:20:38 +0530

    feat(cointable): build UI to display coin market data

--- src/components/CoinTable/CoinTable.jsx
+++ src/components/CoinTable/CoinTable.jsx
@@ -31,19 +31,83 @@ function CoinTable({ currency }) {
     if (data) {
         return (
             <>
-                <div className="bg-sky-300">
-                    CoinTable Component
+
+                <div className="my-5 flex flex-col items-center justify-center gap-5 w-[80vw] mx-auto">
+
+                    {/* Table Header */}
+                    <div className="w-full bg-yellow-400 text-black flex py-4 px-2 font-semibold items-center justify-center">
+                        <div className="basis-[35%]">
+                            Coin
+                        </div>
+                        <div className="basis-[25%]">
+                            Price
+                        </div>
+                        <div className="basis-[20%]">
+                            24h change
+                        </div>
+                        <div className="basis-[20%]">
+                            Market Cap
+                        </div>
+                    </div>
+
+                    {/* Table Body: Maps over the fetched data to render a row for each coin */}
+                    <div className="flex flex-col w-[80vw] mx-auto">
+                        {isLoading && <div>Loading</div>}
+                        {data && data.map((coin) => {
+                            return (
+
+                                <div key={coin.id} className="w-full bg-transparent text-white flex py-4 px-2 font-semibold items-center justify-between">
+
+                                    <div className="flex items-center justify-start gap-3 basis-[35%]">
+
+                                        <div className="w-[5rem] h-[5rem]">
+                                            <img src={coin.image} className="w-full h-full" />
+                                        </div>
+
+                                        <div className="flex flex-col">
+                                            <div className="text-3xl">{coin.name}</div>
+                                            <div className="text-xl">{coin.symbol}</div>
+                                        </div>
+
+                                    </div>
+
+                                    <div className="basis-[25%]">
+                                        {coin.current_price}
+                                    </div>
+
+                                    <div className="basis-[20%]">
+                                        {coin.price_change_24h}
+                                    </div>
+
+                                    <div className="basis-[20%]">
+                                        {coin.market_cap}
+                                    </div>
+
+                                </div>
+
+                            )
+                        })}
+                    </div>
+
+                    {/* Pagination Controls */}
+                    <div className="flex gap-4 justify-center items-center">
+                        <button
+                            onClick={() => { setPage(page - 1) }}
+                            className="btn btn-primary btn-wide text-white text-2xl"
+                            disabled={page === 1}
+                        >
+                            Prev
+                        </button>
+                        <button
+                            onClick={() => { setPage(page + 1) }}
+                            className="btn btn-secondary btn-wide text-white text-2xl"
+                        >
+                            Next
+                        </button>
+                    </div>
+
                 </div>
-                <div>
-                    <button
-                        onClick={() => { setPage(page + 1) }}
-                    >
-                        Click
-                    </button>
-                </div>
-                <div>
-                    page no {page}
-                </div>
+
             </>
         )
     }
