0763f147770e09d4d90b931bb946f29ed9c3e977
Divyanshu Sahu <257divyanshu@gmail.com>
2025-09-16 10:43:15 +0530

    refactor(state): replace currency prop drilling with Context API

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import NavBar from "../components/Navbar/Navbar";
+import Banner from "../components/Banner/Banner";
+import CoinTable from "../components/CoinTable/CoinTable";
+
+function Home() {
+    return (
+        <>
+            <NavBar />
+            <Banner />
+            <CoinTable />
+        </>
+    )
+}
+export default Home;
