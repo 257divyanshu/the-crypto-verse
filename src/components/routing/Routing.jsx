890d26ad45d48ea1f728fa20acd86ade69d7e412
Divyanshu Sahu <257divyanshu@gmail.com>
2025-09-17 11:12:06 +0530

    perf(routing): implement lazy loading for page components

--- src/components/routing/Routing.jsx
+++ src/components/routing/Routing.jsx
@@ -1,7 +1,10 @@
 import {Route, Routes, } from "react-router-dom";
-import Home from "../../pages/Home";
-import CoinDetails from "../../pages/CoinDetails";
 import Layout from "../../pages/Layout";
+import { lazy } from "react";
+// import Home from "../../pages/Home";
+// import CoinDetails from "../../pages/CoinDetails";
+const Home = lazy(()=>import('../../pages/Home'));
+const CoinDetails = lazy(()=>import('../../pages/CoinDetails'));
 
 function Routing(){
     return (
